df02e3460eaee62b514c278b58f87d9997f2867e
王轩 <1825010650@qq.com>
2021-07-06 17:18:27 +0800

    修改demo框架

--- AgileDesign.WpfDemo/MainWindow.xaml.cs
+++ AgileDesign.WpfDemo/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace AgileDesign.WpfDemo
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
---
+++ AgileDesignThemes.Wpf/Themes/Assist/ControlAssist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AgileDesignThemes.Wpf.Themes.Assist
+{
+    public static class ControlAssist
+    {
+        private static readonly CornerRadius DefaultCornerRadius = new(2);
+
+        #region 控制控件的圆角半径。
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+          "CornerRadius", typeof(CornerRadius), typeof(ControlAssist), new PropertyMetadata(DefaultCornerRadius));
+        public static CornerRadius GetCornerRadius(DependencyObject element) => (CornerRadius)element.GetValue(CornerRadiusProperty);
+        /// <summary>
+        /// 控制控件的圆角半径
+        /// Controls the corner radius of the surrounding box.
+        /// </summary>
+        public static void SetCornerRadius(DependencyObject element, CornerRadius value) => element.SetValue(CornerRadiusProperty, value); 
+        #endregion
+
+    }
+}
