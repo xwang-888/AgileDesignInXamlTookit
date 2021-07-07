fb3200d56ca59c9506334980e05f25974fc80145
JAYY513 <JAY_512@OUTLOOK.COM>
2021-07-07 13:56:06 +0800

    PackIconKind 支持直接拿图标字符串数据

---
+++ AgileDesignThemes.Wpf/PackIconDataMarkup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace AgileDesignThemes.Wpf
+{
+    public class PackIconDataMarkup :MarkupExtension
+    {
+        private PackIconKind _packIconKind;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return PackIcon._dataIndex.Value[PackIconKind];
+        }
+
+        [ConstructorArgument("PackIconKind")]
+        public PackIconKind PackIconKind
+        {
+            get => this._packIconKind;
+            set => this._packIconKind = value != null ? value : throw new ArgumentNullException(nameof(value));
+        }
+    }
+}
