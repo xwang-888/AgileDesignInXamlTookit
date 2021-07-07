ea8221f53cd40097047a2ab430506126a757c7ee
JAYY513 <JAY_512@OUTLOOK.COM>
2021-07-07 16:23:49 +0800

    点击水波阴影Button

--- AgileDesignThemes.Wpf/Avatar.cs
+++ AgileDesignThemes.Wpf/Avatar.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
@@ -12,37 +7,42 @@ namespace AgileDesignThemes.Wpf
 {
     public class Avatar : ContentControl
     {
+        private static readonly Effect _effect = new DropShadowEffect
+        {
+            BlurRadius = 8, ShadowDepth = 1.5, Direction = 270, Color = Color.FromArgb(0xaa, 0, 0, 0), Opacity = .42,
+            RenderingBias = RenderingBias.Performance
+        };
+
+        public static readonly DependencyProperty AvatarEffectProperty = DependencyProperty.Register(
+            "AvatarEffect", typeof(Effect), typeof(Avatar), new PropertyMetadata(_effect));
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon", typeof(Brush), typeof(Avatar), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register(
+            "Data", typeof(string), typeof(Avatar), new PropertyMetadata(null));
+
         static Avatar()
         {
             _effect.Freeze();
         }
 
-        private static Effect _effect = new DropShadowEffect() { BlurRadius = 8, ShadowDepth = 1.5, Direction = 270, Color = Color.FromArgb(0xaa, 0, 0, 0), Opacity = .42, RenderingBias = RenderingBias.Performance };
-        public static readonly DependencyProperty AvatarEffectProperty = DependencyProperty.Register(
-            "AvatarEffect", typeof(Effect), typeof(Avatar), new PropertyMetadata(_effect));
-
         public Effect AvatarEffect
         {
-            get { return (Effect)GetValue(AvatarEffectProperty); }
-            set { SetValue(AvatarEffectProperty, value); }
+            get => (Effect) GetValue(AvatarEffectProperty);
+            set => SetValue(AvatarEffectProperty, value);
         }
 
-        public static readonly DependencyProperty IConProperty = DependencyProperty.Register(
-            "ICon", typeof(Brush), typeof(Avatar), new PropertyMetadata(null));
-
-        public Brush ICon
+        public Brush Icon
         {
-            get { return (Brush) GetValue(IConProperty); }
-            set { SetValue(IConProperty, value); }
+            get => (Brush) GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
         }
-
-        public static readonly DependencyProperty DataProperty = DependencyProperty.Register(
-            "Data", typeof(string), typeof(Avatar), new PropertyMetadata(null));
 
         public string Data
         {
-            get { return (string) GetValue(DataProperty); }
-            set { SetValue(DataProperty, value); }
+            get => (string) GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
         }
     }
 }
