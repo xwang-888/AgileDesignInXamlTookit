d40c09b2349517f5bbf70e02c80f92b915be1e8f
JAYY513 <JAY_512@OUTLOOK.COM>
2021-07-08 15:28:29 +0800

    Button进度条样式更改

---
+++ AgileDesignThemes.Wpf/Converters/BorderElement/BorderCircularClipConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace AgileDesignThemes.Wpf.Converters.BorderElement
+{
+    [ValueConversion(typeof(object[]), typeof(Geometry))]
+    public class BorderCircularClipConverter : MarkupExtension,IMultiValueConverter
+    {
+        private static BorderCircularClipConverter borderCircularClipConverter;
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 3 && values[0] is double width && values[1] is double height && values[2] is CornerRadius radius)
+            {
+                if (width < double.Epsilon || height < double.Epsilon)
+                {
+                    return Geometry.Empty;
+                }
+
+                var clip = new RectangleGeometry(new Rect(0, 0, width, height), radius.TopLeft, radius.TopLeft);
+                clip.Freeze();
+
+                return clip;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => borderCircularClipConverter ??= new BorderCircularClipConverter();
+    }
+}
