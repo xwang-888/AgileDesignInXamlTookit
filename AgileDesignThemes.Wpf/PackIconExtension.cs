23153f53e586542c9ffb16bcdc4b60df6e25f917
KI <JAY_512@OUTLOOK.COM>
2022-12-30 15:05:06 +0800

    PackIconExtension

---
+++ AgileDesignThemes.Wpf/PackIconExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace AgileDesignThemes.Wpf
+{
+    /// <summary>
+    /// PackIcon Markup
+    /// </summary>
+    /// <remarks><b>Demo:</b> <i>Content = "{packIcon:PackIconExtension Kind = {x:static packIcon:PackIconKind.Abacus}}"</i></remarks>
+    public class PackIconExtension : MarkupExtension
+    {
+        public PackIconExtension()
+        {
+        }
+
+        public PackIconExtension(PackIconKind kind)
+        {
+            this.Kind = kind;
+        }
+
+        [ConstructorArgument("kind")]
+        public PackIconKind Kind { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new PackIcon() { Kind = Kind };
+        }
+    }
+}
